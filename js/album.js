869db712c238a1bfa8b604b22803b994949713af
nicole07042 <34354969+nicole07042@users.noreply.github.com>
2017-12-07 17:12:40 -0600

    Cho Yhee Commit
    changes

--- js/album.js
+++ js/album.js
@@ -2,9 +2,9 @@
  * This global array holds our collection of albums
  */
 var albums = [{
-  title: "The White Album",
+  title: "The Black Album",
   band:  "The Beatles",
-  year:  1968
+  year:  1978
 }];
 
 
